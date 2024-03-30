3b07d4d291bbc60b40e2e70dc4b242e6479809bc
nox <alexandrearthurpg@gmail.com>
2024-03-30 10:51:30 -0300

    fix: retrieve the objects in Datatable

--- main.py
+++ main.py
@@ -49,35 +49,78 @@ dados = [
 
 
 users = [
-	{
-		"cod": "8054",
-		"nome": "Zelenia Galloway",
-		"email": "risus.quis.diam@outlook.couk"
-	},
-	{
-		"cod": "2482",
-		"nome": "Scarlett Ortiz",
-		"email": "luctus.ut@hotmail.edu"
-	},
-	{
-		"cod": "1431",
-		"nome": "Imogene Phelps",
-		"email": "in@aol.couk"
-	},
-	{
-		"cod": "9306",
-		"nome": "Thomas Lara",
-		"email": "dis@yahoo.couk"
-	},
-	{
-		"cod": "5389",
-		"nome": "Justina Francis",
-		"email": "mollis.phasellus@hotmail.edu"
-	}
+    {
+        "cod": "8054",
+        "nome": "Zelenia Galloway",
+                "email": "risus.quis.diam@outlook.couk"
+    },
+    {
+        "cod": "2482",
+        "nome": "Scarlett Ortiz",
+                "email": "luctus.ut@hotmail.edu"
+    },
+    {
+        "cod": "1431",
+        "nome": "Imogene Phelps",
+                "email": "in@aol.couk"
+    },
+    {
+        "cod": "9306",
+        "nome": "Thomas Lara",
+                "email": "dis@yahoo.couk"
+    },
+    {
+        "cod": "5389",
+        "nome": "Justina Francis",
+                "email": "mollis.phasellus@hotmail.edu"
+    }
 ]
 
+orders = [
+    {
+        "id": "3784",
+        "data": "Oct 2, 2023",
+                "client": "Shana Petersen",
+                "cashier": "Imogene Daugherty",
+                "payment": "Crédito",
+                "order": "Mexico"
+    },
+    {
+        "id": "1547",
+        "data": "Jul 14, 2024",
+                "client": "Carson Kirby",
+                "cashier": "Vladimir Jimenez",
+                "payment": "Dinheiro",
+                "order": "Australia"
+    },
+    {
+        "id": "2192",
+        "data": "Nov 3, 2023",
+                "client": "David Roberson",
+                "cashier": "Martina Stevens",
+                "payment": "Pix",
+                "order": "Germany"
+    },
+    {
+        "id": "2091",
+        "data": "Dec 14, 2023",
+                "client": "Holmes Williams",
+                "cashier": "Mariam Branch",
+                "payment": "Dinheiro",
+                "order": "New Zealand"
+    },
+    {
+        "id": "3194",
+        "data": "Feb 8, 2024",
+                "client": "Garrison Sawyer",
+                "cashier": "Rhoda Salas",
+                "payment": "Dinheiro",
+                "order": "United States"
+    }
+]
 
 # LANDING PAGE
+
 
 @app.page("/")
 def Landing(data: fs.Datasy):
@@ -98,7 +141,7 @@ def Landing(data: fs.Datasy):
                     shape={"": ft.RoundedRectangleBorder(radius=10)},
                     side={"": ft.BorderSide(2, "white54")}
                 ),
-                on_click=lambda x: page.go("/products")
+                on_click=lambda x: page.go("/cashier")
             ),
 
             ft.IconButton(
@@ -122,7 +165,7 @@ def Landing(data: fs.Datasy):
                     shape={"": ft.RoundedRectangleBorder(radius=10)},
                     side={"": ft.BorderSide(2, "white54")}
                 ),
-                on_click=lambda x: page.go("/cashier")
+                on_click=lambda x: page.go("/order")
             ),
         ], alignment="center"
     ),)
@@ -158,32 +201,29 @@ def teste(data: fs.Datasy):
         border_radius=ft.border_radius.all(20),
 
     )
-    
-   
 
-    for index, i in enumerate(dados):
-        cod = ft.Text(i["barCode"])
-        nome = ft.Text(i["title"])
-        obs = ft.Text(i["observation"])
-        price = ft.Text(i["price"])
-        stock = ft.Text(i["stock"])
+    for x in dados:
+        cod = ft.Text(x["barCode"])
+        nome = ft.Text(x["title"])
+        obs = ft.Text(x["observation"])
+        price = ft.Text(x["price"])
+        stock = ft.Text(x["stock"])
 
-        row =  ft.DataRow(
-                cells=[
-                    ft.DataCell(cod),
-                    ft.DataCell(nome),
-                    ft.DataCell(obs),
-                    ft.DataCell(price),
-                    ft.DataCell(stock),
-                ],
-                on_select_changed=lambda e: print(f"{print(e.control.cells)}"),
-                color=({ft.MaterialState.HOVERED: ft.colors.BLUE_GREY_600}),
-                
-            )
-        
+        row = ft.DataRow(
+            cells=[
+                ft.DataCell(cod),
+                ft.DataCell(nome),
+                ft.DataCell(obs),
+                ft.DataCell(price),
+                ft.DataCell(stock),
+            ],
+            on_select_changed=lambda e,i=x: print(i),
+            color=({ft.MaterialState.HOVERED: ft.colors.BLUE_GREY_600}),
+
+        )
 
         place.rows.append(
-           row
+            row
         )
 
     return ft.View(
@@ -194,11 +234,13 @@ def teste(data: fs.Datasy):
             ft.Container(content=place,),
             ft.Row(
                 controls=[
-                    ft.TextButton(icon=ft.icons.ARROW_BACK_OUTLINED,  text=("Voltar"), on_click=lambda x: page.go("/")),
-                    ft.TextButton(icon=ft.icons.BACKPACK,  text=("Adicionar Produto"), on_click=lambda x: page.go("/product/new"))
+                    ft.TextButton(icon=ft.icons.ARROW_BACK_OUTLINED,  text=(
+                        "Voltar"), on_click=lambda x: page.go("/")),
+                    ft.TextButton(icon=ft.icons.BACKPACK,  text=(
+                        "Adicionar Produto"), on_click=lambda x: page.go("/product/new"))
                 ],
                 alignment="spaceAround"
-                
+
             )
 
         ]
@@ -211,18 +253,16 @@ def product_view(data: fs.Datasy):
     return ft.View(
         controls=[
             ft.Text("teste"),
-            ft.TextButton(icon=ft.icons.ARROW_BACK_OUTLINED, text="Voltar", on_click=lambda x: page.go("/teste"))
+            ft.TextButton(icon=ft.icons.ARROW_BACK_OUTLINED,
+                          text="Voltar", on_click=lambda x: page.go("/teste"))
         ]
     )
-
-
-
 
 
 @app.page("/cashier")
 def cashier(data: fs.Datasy):
     page = data.page
-    place = ft.DataTable(
+    df = ft.DataTable(
         columns=[
             ft.DataColumn(ft.Text("Cod"), numeric=True),
             ft.DataColumn(ft.Text("Nome")),
@@ -236,47 +276,104 @@ def cashier(data: fs.Datasy):
         border_radius=ft.border_radius.all(20),
 
     )
-    
-   
 
-    for index, i in enumerate(users):
-        cod = ft.Text(i["cod"])
-        nome = ft.Text(i["nome"])
-        email = ft.Text(i["email"])
+    for x in users:
+        cod = ft.Text(x["cod"])
+        nome = ft.Text(x["nome"])
+        email = ft.Text(x["email"])
 
-        row =  ft.DataRow(
-                cells=[
-                    ft.DataCell(cod),
-                    ft.DataCell(nome),
-                    ft.DataCell(email),
-                ],
-                on_select_changed=lambda e: print(f"{print(e.control.cells)}"),
-                color=({ft.MaterialState.HOVERED: ft.colors.BLUE_GREY_600}),
-                
-            )
-        
+        df.rows.append(ft.DataRow(
+            cells=[
+                ft.DataCell(cod),
+                ft.DataCell(nome),
+                ft.DataCell(email),
+            ],
+             on_select_changed=lambda e,i=x: print(i),
+            color=({ft.MaterialState.HOVERED: ft.colors.BLUE_GREY_600}),
 
-        place.rows.append(
-           row
-        )
+        ))
+
 
     return ft.View(
         horizontal_alignment="center",
         vertical_alignment="spaceBetween",
 
         controls=[
-            ft.Container(content=place,),
+            ft.Container(content=df),
             ft.Row(
                 controls=[
-                    ft.TextButton(icon=ft.icons.ARROW_BACK_OUTLINED,  text=("Voltar"), on_click=lambda x: page.go("/")),
-                    ft.TextButton(icon=ft.icons.ACCOUNT_CIRCLE_SHARP,  text=("Adicionar Caixar"), on_click=lambda x: page.go("/cashier/new"))
+                    ft.TextButton(icon=ft.icons.ARROW_BACK_OUTLINED,  text=(
+                        "Voltar"), on_click=lambda x: page.go("/")),
+                    ft.TextButton(icon=ft.icons.ACCOUNT_CIRCLE_SHARP,  text=(
+                        "Adicionar Caixar"), on_click=lambda x: page.go("/cashier/new"))
                 ],
                 alignment="spaceAround"
-                
+
             )
 
         ]
     )
+
+
+@app.page("/order")
+def order(data: fs.Datasy):
+    page = data.page
+    dt = ft.DataTable(
+        columns=[
+            ft.DataColumn(ft.Text("ID"), numeric=True),
+            ft.DataColumn(ft.Text("Data da Compra")),
+            ft.DataColumn(ft.Text("CLIENTE")),
+            ft.DataColumn(ft.Text("CAIXA")),
+            ft.DataColumn(ft.Text("PAGAMENTO")),
+            ft.DataColumn(ft.Text("RECIBO")),
+        ],
+        rows=[],
+        heading_row_color="red",
+        border=ft.border.all(2, ft.colors.BLACK),
+        border_radius=ft.border_radius.all(20),
+
+    )
+
+    for x in orders:
+
+        dt.rows.append(
+            ft.DataRow(
+                cells=[
+                    ft.DataCell(ft.Text(x["id"])),
+                    ft.DataCell(ft.Text(x["data"])),
+                    ft.DataCell(ft.Text(x["client"])),
+                    ft.DataCell(ft.Text(x["cashier"])),
+                    ft.DataCell(ft.Text(x["payment"])),
+                    ft.DataCell(ft.Text(x["order"])),
+                ],
+                on_select_changed=lambda e,i=x: print(i),
+                color=({ft.MaterialState.HOVERED: ft.colors.BLUE_GREY_600}),
+
+            ))
+
+    return ft.View(
+        horizontal_alignment="center",
+        vertical_alignment="spaceBetween",
+
+        controls=[
+            ft.Container(content=dt),
+            ft.Row(
+                controls=[
+                    ft.TextButton(icon=ft.icons.ARROW_BACK_OUTLINED,  text=(
+                        "Voltar"), on_click=lambda x: page.go("/")),
+                    ft.TextButton(icon=ft.icons.LIST_ALT_OUTLINED,  text=(
+                        "Adicionar Pedido"), on_click=lambda x: page.go("/cashier/new"))
+                ],
+                alignment="spaceAround"
+
+            )
+
+        ]
+    )
+
+
+def ler(dt: ft.DataRow, id):
+    print(orders[id])
 
 
 app.run()
